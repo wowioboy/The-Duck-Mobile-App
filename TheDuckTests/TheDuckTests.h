403f0feb4d8fac1dd7fe21c8983c18d19ca0d277
Lawrence Leach <lawrence@lawrenceleach.com>
2011-11-02 10:49:56 -0700

    Initial Commit

---
+++ TheDuckTests/TheDuckTests.h
@@ -0,0 +1,13 @@
+//
+//  TheDuckTests.h
+//  TheDuckTests
+//
+//  Created by Lawrence Leach on 11/2/11.
+//  Copyright (c) 2011 Alliance Acquisitions, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TheDuckTests : SenTestCase
+
+@end
---
+++ TheDuckTests/TheDuckTests.m
@@ -0,0 +1,32 @@
+//
+//  TheDuckTests.m
+//  TheDuckTests
+//
+//  Created by Lawrence Leach on 11/2/11.
+//  Copyright (c) 2011 Alliance Acquisitions, Inc. All rights reserved.
+//
+
+#import "TheDuckTests.h"
+
+@implementation TheDuckTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TheDuckTests");
+}
+
+@end
