c06c26e339c580bb7b8007df178205accb5bcbfa
Lawrence Leach <lawrence@lawrenceleach.com>
2011-11-02 17:02:09 -0700

    Initial Commit

---
+++ TheDuckWebcomics/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TheDuckWebcomics
+//
+//  Created by Lawrence Leach on 11/2/11.
+//  Copyright (c) 2011 Alliance Acquisitions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TheDuckWebcomics/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheDuckWebcomics
+//
+//  Created by Lawrence Leach on 11/2/11.
+//  Copyright (c) 2011 Alliance Acquisitions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
